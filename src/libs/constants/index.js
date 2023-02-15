286675fe4e691ce2da510b3bb6e85c17a631b545
Lyutink <krivenko.lyuda@gmail.com>
2023-02-15 20:44:42 +0300

    media

---
+++ src/libs/constants/index.js
@@ -0,0 +1,8 @@
+export const breakPoints = {
+  PHONE: 320,
+  TABLET: 767.98,
+  DESKTOP: 1280,
+};
+
+// export const baseURL = ' http://localhost:8080/api';
+// //'https://slim-moms.herokuapp.com/api';
